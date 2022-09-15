1117885ba8f5937fc582e4606a6dc550bb42efd5
Deborah Bussolo <deborahbussolo@gmail.com>
2022-09-15 16:54:05 -0300

    functions 0

---
+++ funções/functions.js
@@ -0,0 +1,8 @@
+// declaração da função
+function createPhrases() {
+    console.log("Estudar é muito bom")
+    console.log("Paciência e persistência")
+    console.log("Revisão é mãe do aprendizado")
+}
+// executa a função
+createPhrases()
